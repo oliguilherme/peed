d0e19768a947e382af0183c735365498beca4b04
oliguilherme <guilhermeleitesouza2018@gmail.com>
2025-05-20 11:36:15 -0300

    adiciona programa subtring

---
+++ substring/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+char * is_substring(char *haystack, char *needle);
+
+int main() {
+    char *haystack = "cacarroca";
+    char *needle = "carro";
+
+    printf("Esta eh a string: %s", is_substring(haystack, needle));
+
+    return 0;
+}
+
+char * is_substring(char *haystack, char *needle) {
+    char *start = needle;
+
+    while (*haystack) {
+        char *h = haystack, *n = needle;
+
+        while (*h && *n && *h == *n) {
+            h++; n++;
+        }
+        if (*n == '\0') return start;
+
+        haystack++;
+    }
+    return NULL;
+}
