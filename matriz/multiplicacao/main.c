f0a54d737617ab824279047830040ae574289804
oliguilherme <guilhermeleitesouza2018@gmail.com>
2025-05-21 19:08:21 -0300

    atualizacao de multiplicacao

--- matriz/multiplicacao/main.c
+++ matriz/multiplicacao/main.c
@@ -3,39 +3,52 @@
 #include <time.h>
 
 int main() {
-    int matriz_a[3][2], matriz_b[2][3], mult[3][3], soma = 0;
-    srand(time(NULL));
+	int m, n, p, q;
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 2; j++) {
-            matriz_a[i][j] = rand() % 10;
-            printf("%d ", matriz_a[i][j]);
-        }
-        printf("\n");
-    }
+    printf("Entre com os numeros de linhas e de colunas da primeira matriz: ");
+    scanf("%d %d", &m, &n);
+
+    printf("\nEntre com os numeros de linhas e de coluncas da segunda matriz: ");
+    scanf("%d %d", &p, &q);
 
     printf("\n");
+    if (n == p) {
+        int matriz_a[m][n], matriz_b[p][q], mult[m][q], soma = 0;
+        srand(time(NULL));
 
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 3; j++) {
-            matriz_b[i][j] = rand() % 10;
-            printf("%d ", matriz_b[i][j]);
-        }
-        printf("\n");
-    }
-
-    printf("\n");
-
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            soma = 0;
-            for (int k = 0; k < 2; k++) {
-                soma += matriz_a[i][k] * matriz_b[k][j];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matriz_a[i][j] = rand() % 10;
+                printf("%d ", matriz_a[i][j]);
             }
-            mult[i][j] = soma;
-            printf("%d ", mult[i][j]);
+            printf("\n");
         }
+
         printf("\n");
+
+        for (int i = 0; i < p; i++) {
+            for (int j = 0; j < q; j++) {
+                matriz_b[i][j] = rand() % 10;
+                printf("%d ", matriz_b[i][j]);
+            }
+            printf("\n");
+        }
+
+        printf("\n");
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < q; j++) {
+                soma = 0;
+                for (int k = 0; k < n; k++) {
+                    soma += matriz_a[i][k] * matriz_b[k][j];
+                }
+                mult[i][j] = soma;
+                printf("%d ", mult[i][j]);
+            }
+            printf("\n");
+        }
+    } else {
+        printf("Nao eh possivel calcular a multiplicacao entre essas matrizes");
     }
 
     return 0;
